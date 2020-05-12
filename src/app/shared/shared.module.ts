e874114831550cbc196296597e3a98f5b2520e4b
Vincent Roth <vincent@placeme.io>
2020-05-12 17:02:31 +0200

    Pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UppercaseDirective } from './uppercase.directive';
+import { TruncatePipe } from './truncate.pipe';
 
 @NgModule({
-  declarations: [UppercaseDirective],
-  exports: [UppercaseDirective],
+  declarations: [UppercaseDirective, TruncatePipe],
+  exports: [UppercaseDirective, TruncatePipe],
   imports: [CommonModule],
 })
 export class SharedModule {}

b0f6703c98111f4f358962c7f81283cb43f6e668
Vincent Roth <vincent@placeme.io>
2020-05-13 16:56:13 +0200

    Router - child routes

--- src/app/animal/animal.module.ts
+++ src/app/animal/animal.module.ts
@@ -4,14 +4,16 @@ import { AnimalDetailComponent } from './animal-detail/animal-detail.component';
 import { AnimalListComponent } from './animal-list/animal-list.component';
 import { SharedModule } from '../shared/shared.module';
 import { AnimalItemComponent } from './animal-item/animal-item.component';
-import { RouterModule } from '@angular/router';
+import { AnimalRootComponent } from './animal-root/animal-root.component';
+import { AnimalRoutingModule } from './animal-routing.module';
 
 @NgModule({
   declarations: [
     AnimalDetailComponent,
     AnimalListComponent,
     AnimalItemComponent,
+    AnimalRootComponent,
   ],
-  imports: [CommonModule, SharedModule, RouterModule],
+  imports: [CommonModule, SharedModule, AnimalRoutingModule],
 })
 export class AnimalModule {}

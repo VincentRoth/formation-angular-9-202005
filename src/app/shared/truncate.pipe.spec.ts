232db38463fe5cdb6d23c4dc8ddff63e57a6cc6b
Vincent Roth <vincent@placeme.io>
2020-05-15 13:18:55 +0200

    Test unitaire - pipe

--- src/app/shared/truncate.pipe.spec.ts
+++ src/app/shared/truncate.pipe.spec.ts
@@ -1,8 +1,25 @@
 import { TruncatePipe } from './truncate.pipe';
 
 describe('TruncatePipe', () => {
+  let pipe: TruncatePipe;
+
+  beforeEach(() => {
+    pipe = new TruncatePipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new TruncatePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('null value', () => {
+    expect(pipe.transform(null)).toEqual(null);
+  });
+
+  it('text value inferior to limit', () => {
+    expect(pipe.transform('mon texte', 5)).toEqual('mo...');
+  });
+
+  it('text value superior to limit', () => {
+    expect(pipe.transform('mon texte', 10)).toEqual('mon texte');
   });
 });

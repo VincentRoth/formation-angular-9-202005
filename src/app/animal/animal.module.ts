219678e70dc130a40f98a6ad3af4d0cb15bbbfee
Vincent Roth <vincent@placeme.io>
2020-05-15 14:44:14 +0200

    Angular material

--- src/app/animal/animal.module.ts
+++ src/app/animal/animal.module.ts
@@ -8,6 +8,8 @@ import { AnimalRootComponent } from './animal-root/animal-root.component';
 import { AnimalRoutingModule } from './animal-routing.module';
 import { AnimalFormComponent } from './animal-form/animal-form.component';
 import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,13 @@ import { FormsModule } from '@angular/forms';
     AnimalRootComponent,
     AnimalFormComponent,
   ],
-  imports: [CommonModule, SharedModule, AnimalRoutingModule, FormsModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    AnimalRoutingModule,
+    FormsModule,
+    MatCardModule,
+    MatButtonModule,
+  ],
 })
 export class AnimalModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AnimalModule } from './animal/animal.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AnimalModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AnimalModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
